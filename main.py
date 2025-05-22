eab219012b9798a251e4cf4394ca938acfb6d95a
Adan <adanmm99@gmail.com>
2025-05-22 09:38:01 +0300

    updated rule value

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ except Exception as e:
     sys.exit(1)
 
 # === CONSTANTS ===
-RULE_VALUE = '-is:retweet ("who would you vote for" OR "would you vote for" OR "if elections were held today" OR "president Kenya" OR Ruto OR Matiang\'i OR place:KE)'   # Added candidate names
+RULE_VALUE = '-is:retweet ("who would you vote for" OR "would you vote for" OR "if elections were held today" OR "president of kenya" OR" president Kenya" OR Ruto OR Matiang\'i OR place:KE)'   # Added candidate names
 CANDIDATE_NAMES = ["yes","no","fred matiangi","Ruto", "William Ruto", "Rigathi", "Gachagua", "Matiangi", "Musyoka", "Omtatah", "Maraga", "Kalonzo","Matiang'i"]
 BLOCKLIST = ["movie", "food", "sport", "city", "music","scotland","united states","trump","USA","US","ireland","wales"]  # Re-enabled blocklist
 HEADERS = {'Authorization': f'Bearer {BEARER_TOKEN}', 'Content-type': 'application/json'}
